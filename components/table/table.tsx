ff9b9dbbb88de400bcb0717727ff440ec1a1d4c7
Paweł <pawel_gnat@o2.pl>
2023-12-10 15:57:11 +0100

    refactor: remove unnecessary props from table component

--- components/table/table.tsx
+++ components/table/table.tsx
@@ -1,9 +1,5 @@
 'use client';
 
-import { useContext, useEffect } from 'react';
-
-import { ModalSheetContext } from '@/context/modal-sheet-context';
-
 import {
   Table as TableUI,
   TableBody,
@@ -19,41 +15,9 @@ interface ManageTableProps {
   data: SafeEmployee[] | Department[] | Location[] | SafeSettlement[];
   headers: string[];
   rows: any;
-  employees?: SafeEmployee[];
-  settlements?: SafeSettlement[];
-  locations?: Location[];
-  departments?: Department[];
 }
 
-const Table: React.FC<ManageTableProps> = ({
-  title,
-  data,
-  headers,
-  rows,
-  employees,
-  settlements,
-  locations,
-  departments,
-}) => {
-  const { setLocations, setDepartments, setEmployees, setSettlements } =
-    useContext(ModalSheetContext);
-
-  useEffect(() => {
-    if (locations) setLocations(locations);
-  }, [locations, setLocations]);
-
-  useEffect(() => {
-    if (departments) setDepartments(departments);
-  }, [departments, setDepartments]);
-
-  useEffect(() => {
-    if (employees) setEmployees(employees);
-  }, [employees, setEmployees]);
-
-  useEffect(() => {
-    if (settlements) setSettlements(settlements);
-  }, [settlements, setSettlements]);
-
+const Table: React.FC<ManageTableProps> = ({ title, data, headers, rows }) => {
   if (data.length === 0) {
     return (
       <p className="text-center">
